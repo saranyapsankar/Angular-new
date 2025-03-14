53270d074c954a16dc1b83d48b12fb6c39957ee3
“Saranya” <saranyapsankar@gmail.com>
2025-03-14 17:19:57 +0530

    15 to 16 update

--- src/app/shared/services/auth-guard.ts
+++ src/app/shared/services/auth-guard.ts
@@ -1,19 +1,14 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService, private router: Router) {}
-
-  canActivate(): boolean {
-    if (this.authService.getAuthStatus()) {
-      return true;
-    } else {
-      this.router.navigate(['/login']);
-      return false;
-    }
+export const AuthGuard = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  if (authService.getAuthStatus()) {
+    return true;
+  } else {
+    router.navigate(['/login']);
+    return false;
   }
 }
